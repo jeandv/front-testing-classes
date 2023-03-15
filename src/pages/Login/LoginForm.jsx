abc6cf201d39c32efcd28a9454e14706edf423a0
Jean Rondon <jeanrdev@gmail.com>
2023-03-15 13:58:23 -0400

    use resgister, handleSubmit, watch, formState (errors, isDirty, isValid), reset from useForm of react hook form

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -3,14 +3,20 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { loginFormSchema } from './schemas/login-form-schema';
 
 export default function LoginForm() {
-  useForm({
+  const {
+    register, // registra los inputs con las propiedades del formulario
+    handleSubmit, // hace el submit del form
+    watch, // si modificamos algun valor del form, ese valor se guardara y cada vez q se cambia se podrá usar
+    formState: { errors, isDirty, isValid }, // mostramos los errores, si es valido, etc
+    reset, // hace reset al form
+  } = useForm({
     defaultValues: { username: '', password: '' },
     mode: 'onChange',
     resolver: yupResolver(loginFormSchema),
   });
 
   return (
-    <form>
+    <form onSubmit={handleSubmit()}>
       <input type='text' name='' id='' />
       <input type='text' name='' id='' />
       <button></button>
