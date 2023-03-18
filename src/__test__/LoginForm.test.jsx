9d236b9b4ccc7e6521f466478de5223d9a5d5870
Jean Rondon <jeanrdev@gmail.com>
2023-03-18 00:32:33 -0400

    add test

--- src/__test__/LoginForm.test.jsx
+++ src/__test__/LoginForm.test.jsx
@@ -1,11 +1,12 @@
-const { render, cleanup, screen } = require('@testing-library/react');
+const { render, cleanup, screen, waitFor } = require('@testing-library/react');
 import { LoginForm } from '../pages';
+import { LoginFormMock } from '../_mocks_/LoginForm.mock';
 
 describe('LoginForm', () => {
-  afterEach(cleanup);
-  afterEach(jest.clearAllMocks);
+  // afterEach(cleanup);
+  // afterEach(jest.clearAllMocks);
 
-  beforeAll(() => {
+  beforeEach(() => {
     render(<LoginForm />);
   });
 
@@ -22,7 +23,25 @@ describe('LoginForm', () => {
     expect(usernameInput).toBeInTheDocument();
     // expect(passwordInput).toBeInTheDocument();
 
-    // expect(usernameInput).toHaveValue('');
+    expect(usernameInput).toHaveValue('');
     // expect(passwordInput).toHaveValue('');
+  });
+
+  it('should enable the submit button if the form values are valid', async () => {
+    const usernameInput = screen.getByRole('textbox', { name: 'Username' });
+    // const passwordInput = screen.getByRole('textbox', { name: 'Password' });
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+
+    expect(submitButton).toBeDisabled();
+
+    await userEvent.type(usernameInput, LoginFormMock.username);
+    // await userEvent.type(passwordInput, LoginFormMock.password);
+
+    await waitFor(() => {
+      expect(usernameInput).toHaveValue(LoginFormMock.username);
+      // expect(passwordInput).toHaveValue(LoginFormMock.password);
+    });
+
+    expect(submitButton).toBeDisabled();
   });
 });
